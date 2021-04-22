a0fcb0cc9aea72a5ba9d29be59f9a4c05a239b84
Mateusz <you@example.com>
2021-04-22 12:56:40 +0200

    testy początek

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,7 @@
 
 const request = require('supertest');
 const app = require('../index');
+jest.mock('express')
 
 describe('Test the root path', () =>{
     test('It should response the GET method', async()=>{
